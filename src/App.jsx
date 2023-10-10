d1190ffd6e2f5dd454b2030329519c137f0fef2d
Earl Funk <earlfunk21@gmail.com>
2023-10-10 20:44:32 +0800

    Alternative codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,9 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function App() {
 	const [code, setCode] = useState("");
-
-	return (
-		<div className="Center">
-			<h1>Enter a game code</h1>
-			<input
-				type="text"
-				id="inputCode"
-				onChange={e => setCode(e.target.value)}
-				value={code}
-			/>
-			<Link
-				className="btn-primary"
-				to="/bingo"
-				state={code}>
-				Get Card
-			</Link>
-		</div>
-	);
-}
-
-function Bingo() {
 	const { state } = useLocation();
 	const [board, setBoard] = useState({
 		B: [],
@@ -33,15 +12,7 @@ function Bingo() {
 		G: [],
 		O: [],
 	});
-	const [disabledNum, setDisabledNum] = useState({
-		B: [],
-		I: [],
-		N: [],
-		G: [],
-		O: [],
-	});
 	const [token, setToken] = useState("");
-	const navigate = useNavigate();
 
 	const checkCard = async () => {
 		const response = await axios.get("http://www.hyeumine.com/checkwin.php", {
@@ -52,71 +23,83 @@ function Bingo() {
 	};
 
 	const newCard = async () => {
-		setDisabledNum({
-			B: [],
-			I: [],
-			N: [],
-			G: [],
-			O: [],
-		});
 		const response = await axios.get("http://www.hyeumine.com/getcard.php", {
 			params: { bcode: state },
 		});
 		if (response.data === 0) {
 			alert(`Invalid Code: ${state}`);
-			return navigate(-1);
 		}
 		setBoard(response.data.card);
 		setToken(response.data.playcard_token);
 	};
 
-	useEffect(() => {
-		newCard();
-	}, []);
-
-	return (
-		<div className="Center">
-			<h1>Game Code: {state}</h1>
-			<Link to="/">Enter another code</Link>
-			<Link to={`http://www.hyeumine.com/bingodashboard.php?bcode=${state}`}>
-				Go to Dashboard
-			</Link>
-			<div className="board">
-				{Object.entries(board).map(([letter, nums], key) => (
-					<div
-						key={key}
-						className="board-col">
-						{letter}
-						{nums.map((num, numKey) => (
-							<button
-								key={numKey}
-								className="btn-num"
-								onClick={e => {
-									setDisabledNum(prev => ({
-										...prev,
-										[letter]: [...prev[letter], num],
-									}));
-								}}
-								disabled={disabledNum[letter].includes(num)}>
-								{num}
-							</button>
-						))}
-					</div>
-				))}
+	const enterGameCode = () => {
+		return (
+			<div className="Center">
+				<h1>Enter a game code</h1>
+				<input
+					type="text"
+					id="inputCode"
+					onChange={e => setCode(e.target.value)}
+					value={code}
+				/>
+				<Link
+					className="btn-primary"
+					to="/"
+					state={code}>
+					Get Card
+				</Link>
 			</div>
-			<button
-				onClick={checkCard}
-				className="btn-primary">
-				Check Card
-			</button>
-			<button
-				onClick={newCard}
-				className="btn-primary">
-				New Card
-			</button>
-		</div>
-	);
+		);
+	};
+
+  useEffect(() => {
+    if(state)
+      newCard()
+  }, [state])
+
+	const gameBoard = () => {
+		return (
+			<div className="Center">
+				<h1>Game Code: {code}</h1>
+				<Link to="/">Enter another code</Link>
+				<Link to={`http://www.hyeumine.com/bingodashboard.php?bcode=${code}`}>
+					Go to Dashboard
+				</Link>
+				<div className="board">
+					{Object.entries(board).map(([letter, nums], key) => (
+						<div
+							key={key}
+							className="board-col">
+							{letter}
+							{nums.map((num, numKey) => (
+								<button
+									key={numKey}
+									className="btn-num"
+									onClick={e => {
+										e.currentTarget.disabled = true;
+									}}>
+									{num}
+								</button>
+							))}
+						</div>
+					))}
+				</div>
+				<button
+					onClick={checkCard}
+					className="btn-primary">
+					Check Card
+				</button>
+				<button
+					onClick={newCard}
+					className="btn-primary">
+					New Card
+				</button>
+			</div>
+		);
+	};
+
+	return state ? gameBoard() : enterGameCode();
 }
 
 export default App;
-export { Bingo };
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App, { Bingo } from "./App.jsx";
+import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
@@ -10,10 +10,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 			<Route
 				path="/"
 				element={<App />}
-			/>
-			<Route
-				path="/bingo"
-				element={<Bingo />}
 			/>
 		</Routes>
 	</BrowserRouter>
